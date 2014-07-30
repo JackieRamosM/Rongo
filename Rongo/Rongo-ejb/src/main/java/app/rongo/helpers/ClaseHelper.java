4ea32646f0bac912225bdb000e86b29084c6dcfd
Jacqueline Ramos <jackie.ljrm@gmail.com>
2014-07-30 18:43:04 -0500

    added conection pool

---
+++ Rongo/Rongo-ejb/src/main/java/app/rongo/helpers/ClaseHelper.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.rongo.helpers;
+
+import app.rongo.persistence.Clase;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Liliana
+ */
+@Stateless
+@LocalBean
+public class ClaseHelper {
+
+    @PersistenceContext(unitName = "app.rongo_Rongo-ejb_ejb_1.0-SNAPSHOTPU")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
+
+    public List<Clase> getClases() {
+        Query query = em.createNamedQuery("Clase.findAll");  
+        return query.getResultList(); 
+    }
+
+    public Clase update(Clase clase) {
+        return em.merge(clase);  
+    }
+
+  /*  public void persist(Object object) {
+        try {
+            utx.begin();
+            em.persist(object);
+            utx.commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            throw new RuntimeException(e);
+        }
+    }*/
+
+}
