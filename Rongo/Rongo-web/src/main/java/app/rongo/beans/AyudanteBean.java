d96373c1f2a68fee09428060de5b3f1425f085d1
Jacqueline Ramos <jackie.ljrm@gmail.com>
2014-08-29 11:31:10 -0500

    listing ayudantias

--- Rongo/Rongo-web/src/main/java/app/rongo/beans/AyudanteBean.java
+++ Rongo/Rongo-web/src/main/java/app/rongo/beans/AyudanteBean.java
@@ -3,14 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package app.rongo.beans;
 
 import app.rongo.Session;
 import app.rongo.facade.AyudanteFacadeLocal;
 import app.rongo.facade.EstudianteFacadeLocal;
 import app.rongo.persistence.Ayudante;
+import app.rongo.persistence.Ayudantia;
 import app.rongo.persistence.Estudiante;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.Dependent;
 import javax.faces.context.FacesContext;
@@ -25,49 +27,95 @@ import javax.inject.Named;
 public class AyudanteBean {
 
     @EJB
-    AyudanteFacadeLocal ayudanteFacade;
+    private AyudanteFacadeLocal ayudanteFacade;
     @EJB
-    EstudianteFacadeLocal estudianteFacade;
+    private EstudianteFacadeLocal estudianteFacade;
+    private List<Estudiante> estudiantes;
     private final Session session = new Session();
-    
+    private final String usuario = session.getUser();
+    private Estudiante estudiante;
+    private List<Ayudante> ayudantes = new ArrayList<Ayudante>();
+    private List<Ayudantia> ayudantiasdelayudante = new ArrayList<Ayudantia>();
+
     /**
      * Creates a new instance of AyudanteBean
      */
     public AyudanteBean() {
     }
+
+    public void findEstudiante() {
+        estudiantes = new ArrayList<Estudiante>();
+        estudiantes = estudianteFacade.findAll();
+        for (Estudiante e : estudiantes) {
+            if (e.getUsuario().equals(usuario)) {
+                estudiante = e;
+                break;
+            }
+        }
+
+    }
+
+    public void obtenerAyudantiasDelAyudante() {
+        findEstudiante();
+        ayudantes = ayudanteFacade.findAll();
+        for (Ayudante a : ayudantes) {
+            if (a.getIdEstudiante().equals(estudiante)) {
+                ayudantiasdelayudante.add(a.getIdayudantia());
+                System.out.println("ayudantia=" + a.getIdayudantia().getNombre() + " cod="+session.codigoService(a.getIdayudantia().getNombre()));
+                
+            }
+        }
+    }
     
+    public String obtenermateria(String cod){
+        System.out.println("cod="+cod + "" +session.codigoService(cod));
+        return session.codigoService(cod);
+    }
+
     public String getAyudanteName(int id) {
         Ayudante a = ayudanteFacade.find(id);
         Estudiante e = estudianteFacade.find(a.getIdEstudiante().getIdUsuario());
         return session.nombreService(session.matriculaService(e.getUsuario()));
     }
-    
-    public String getAyudanteNombre(){
+
+    public String getAyudanteNombre() {
         int id = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("b"));
         Ayudante a = ayudanteFacade.find(id);
         Estudiante e = estudianteFacade.find(a.getIdEstudiante().getIdUsuario());
         return session.nombreService(session.matriculaService(e.getUsuario()));
     }
-    
+
     public String getAyudanteUser(int id) {
         Ayudante a = ayudanteFacade.find(id);
         Estudiante e = estudianteFacade.find(a.getIdEstudiante().getIdUsuario());
         return e.getUsuario();
     }
-    
-    public int getIdAyudante(){
+
+    public int getIdAyudante() {
         return Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("b"));
     }
-    
+
     public Estudiante getEstudiante() {
         int id = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("b"));
         Ayudante a = ayudanteFacade.find(id);
         return estudianteFacade.find(a.getIdEstudiante().getIdUsuario());
     }
-    
-    public String getEstudianteMateria(){
+
+    public String getEstudianteMateria() {
         int id = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("b"));
         Ayudante a = ayudanteFacade.find(id);
         return session.carreraService(session.matriculaService(a.getIdEstudiante().getUsuario()));
+    }
+
+    public List<Ayudantia> getAyudantiasdelayudante() {
+        return ayudantiasdelayudante;
+    }
+
+    public void setAyudantiasdelayudante(List<Ayudantia> ayudantiasdelayudante) {
+        this.ayudantiasdelayudante = ayudantiasdelayudante;
+    }
+
+    public Session getSession() {
+        return session;
     }
 }
