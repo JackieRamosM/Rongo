1764862e7f4227ce89360bfb1bffcf6482be2212
DenkSchuldt <dschuldt@outlook.com>
2014-08-29 15:02:12 -0500

    Deleting unuseful methods in ClaseBean

--- Rongo/Rongo-web/src/main/java/app/rongo/beans/ClaseBean.java
+++ Rongo/Rongo-web/src/main/java/app/rongo/beans/ClaseBean.java
@@ -43,57 +43,15 @@ public class ClaseBean implements Serializable {
     }
     
     public List<Clase> getClasesAyudanteMateria(int idAyudante, int idAyudantia){
-        int index = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("c"));
         clases = clase.findAll();
         Iterator<Clase> i = clases.iterator();
         while (i.hasNext()) {
             try{
                 if(!i.next().getIdayudantia().getIdayudantia().equals(idAyudantia)) i.remove();
+                if(!i.next().getIdayudante().equals(idAyudante)) i.remove();
             }catch(Error err){}
-        }
-        if(index < 0) {
-            for(int k=index; k==0; k++) {
-            }
-        }
-        else if(index > 0) {
-            for(int k=index; k==0; k--) {
-            }
-        }
-        else {
-            Calendar cal = Calendar.getInstance();
-        }
+        }        
         return clases;
-    }
-    
-    public String getWeek(){
-        int index = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("c"));
-        String week = "";
-        if(index < 0) {
-            Calendar cal = Calendar.getInstance(Locale.US);
-            cal.set(Calendar.WEEK_OF_YEAR, 0);
-            cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-            for(int k=index; k==0; k++) cal.add(Calendar.DAY_OF_MONTH, -7);
-            week = "Semana del " + new SimpleDateFormat("d").format(cal.getTime()) + " al ";
-            cal.set(Calendar.DAY_OF_WEEK, 6);
-            week = week + new SimpleDateFormat("d").format(cal.getTime()) + " de " + new SimpleDateFormat("MMMM").format(cal.getTime()) + " de " + new SimpleDateFormat("YYYY").format(cal.getTime());
-            
-        }
-        else if(index > 0) {
-            for(int k=index; k==0; k--) {
-            }
-        }
-        else {
-            Calendar cal = Calendar.getInstance(Locale.US);
-            cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-            week = "Semana del " + new SimpleDateFormat("d").format(cal.getTime()) + " al ";
-            cal.set(Calendar.DAY_OF_WEEK, 6);
-            week = week + new SimpleDateFormat("d").format(cal.getTime()) + " de " + new SimpleDateFormat("MMMM").format(cal.getTime()) + " de " + new SimpleDateFormat("YYYY").format(cal.getTime());
-        }
-        return week;
-    }
-    
-    public int getIndex() {
-        return Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("c"));
     }
     
     public List<Clase> getClases(){
